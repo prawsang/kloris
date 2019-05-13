1e4edfbb00792caf3299f05691a54aa9f5aece45
Prawsang <prawcha.p@gmail.com>
2019-05-13 13:01:03 +0700

    make nav sticky

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,9 +11,17 @@ import PropTypes from "prop-types"
 import Nav from "./nav"
 // import Footer from "./footer"
 import "../style/index.scss"
+import logo from "../assets/web/logo.svg"
+import lang from "../assets/web/th.png"
 
 const Layout = ({ children }) => (
   <>
+    <div className="nav-head">
+      <div className="container">
+        <img className="lang" src={lang} />
+        <img className="logo" src={logo} />
+      </div>
+    </div>
     <Nav />
     {children}
     {/* <Footer /> */}
