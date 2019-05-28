1e3d8830de50175ad5da3d80d0a603d895a93cd1
Prawsang <prawcha.p@gmail.com>
2019-05-29 00:07:16 +0700

    adjust language switcher

--- src/components/lang.js
+++ src/components/lang.js
@@ -1,12 +1,16 @@
 import React from "react"
 import th from "../assets/web/th.png"
+import en from "../assets/web/en.png"
 import Link from "gatsby-link"
 
 const LanguageSelect = ({ langs, currentLangKey, defaultLangKey }) => {
   const altLangs = langs.filter(e => e !== defaultLangKey)
   return (
-    <Link to={currentLangKey !== defaultLangKey ? "/" : `/${altLangs[0]}`}>
-      <img src={th} alt="" className="lang" />
+    <Link
+      to={currentLangKey !== defaultLangKey ? "/" : `/${altLangs[0]}`}
+      className="lang"
+    >
+      <img src={currentLangKey !== defaultLangKey ? en : th} alt="" />
     </Link>
   )
 }
