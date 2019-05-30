d55a17fb2196ec2005497e7b43533621bf2200b2
Prawsang <prawcha.p@gmail.com>
2019-05-30 21:52:42 +0700

    configure nav links

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,29 +3,40 @@ import React from "react"
 import { FormattedMessage } from "react-intl"
 import { injectIntl, intlShape } from "react-intl"
 
-const Nav = ({ className, intl }) => {
+import { Link } from "gatsby"
+
+const NavLink = ({ link, id, intl }) => {
+  const formatLink = link => {
+    const prefix = intl.locale === "th" ? "/th" : ""
+    return prefix + link
+  }
+
   const textStyle = {
     fontSize: intl.locale === "th" ? "0.85em" : "0.75em",
     letterSpacing: intl.locale === "th" ? "0.5pt" : "1pt",
   }
   return (
+    <Link to={formatLink(link)}>
+      <li className="menu-item" style={textStyle}>
+        <FormattedMessage id={id} />
+      </li>
+    </Link>
+  )
+}
+
+const Nav = ({ className, intl }) => {
+  return (
     <nav className={className}>
       <ul className="menu">
-        <li className="menu-item" style={textStyle}>
-          <FormattedMessage id="home" />
-        </li>
-        <li className="menu-item" style={textStyle}>
-          <FormattedMessage id="fromNatureToSkin" />
-        </li>
-        <li className="menu-item" style={textStyle}>
-          <FormattedMessage id="intenseHealing" />
-        </li>
-        <li className="menu-item" style={textStyle}>
-          <FormattedMessage id="support" />
-        </li>
-        <li className="menu-item" style={textStyle}>
-          <FormattedMessage id="contactUs" />
-        </li>
+        <NavLink link="/" id="home" intl={intl} />
+        <NavLink
+          link="/from-nature-to-skin"
+          id="fromNatureToSkin"
+          intl={intl}
+        />
+        <NavLink link="/intense-healing" id="intenseHealing" intl={intl} />
+        <NavLink link="/support" id="support" intl={intl} />
+        <NavLink link="/contact-us" id="contactUs" intl={intl} />
       </ul>
     </nav>
   )
