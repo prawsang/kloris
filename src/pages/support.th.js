28be3f7a04a7236fd1ce68859af0dab9876ddfc4
Prawsang <prawcha.p@gmail.com>
2019-05-31 23:38:14 +0700

    add thai support page

---
+++ src/pages/support.th.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+import Layout from "../layout/th"
+import Support from "../components/support"
+import SEO from "../components/seo"
+
+import support from "../data/messages/support/th"
+import general from "../data/messages/th"
+
+const SupportPage = props => (
+  <Layout location={props.location} i18nMessages={{ ...general, ...support }}>
+    <SEO title="Support" keywords={[`gatsby`, `application`, `react`]} />
+    <Support />
+  </Layout>
+)
+
+export default SupportPage
