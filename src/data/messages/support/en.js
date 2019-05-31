110736d0d303ff13cdf9c6949eb3f9a5b36ecf86
Prawsang <prawcha.p@gmail.com>
2019-05-31 18:17:23 +0700

    add support messages for both languages

---
+++ src/data/messages/support/en.js
@@ -0,0 +1,46 @@
+module.exports = {
+  shipping: "Shipping",
+  payment: "Payment",
+  orders: "Orders",
+  shipping1: "What shipping companies are you using?",
+  shipping1a:
+    "For shipping in Bangkok Metropolitan Region, we use Kerry Express. If you are shipping to other provinces in Thailand, the items will be shipped to you using Thailand Post’s EMS service.",
+  shipping2: "How much are the shipping costs?",
+  shipping2a: "We offer FREE SHIPPING for all orders.",
+  shipping3: "Do you offer international shipping?",
+  shipping3a: "Currently, no. We do not sell our products outside of Thailand.",
+  shipping4: "How long will it take for my items to arrive?",
+  shipping4a:
+    "The items will arrive in 1-2 days for Bangkok Metropolitan Region and 3-5 days for other provinces in Thailand.",
+  shipping5: "What if my item is lost or damaged during shipping?",
+  shipping5a:
+    "If your items arrive damaged, or if you think your items are lost during shipping, please contact us via phone for further assistance. Our phone number is +6694-994-8652",
+  shipping6: "I want to return my item. Is it possible?",
+  shipping6a:
+    "No. Items cannot be returned for any reason. However, if your item is damaged during shipping, please contact us via phone at +6694-994-8652",
+  payment1: "How can I pay for my orders?",
+  payment1a:
+    "You can pay for your orders via direct bank transfer. Please see our bank details for our account number, name, and bank name. Once you have completed your payment, please send your proof of payment to LINE @kloris or our email",
+  bankInformation: "Bank information",
+  bankName: "Bank Name",
+  accountName: "Account Name",
+  accountNumber: "Account Number",
+  bangkokBank: "Bangkok Bank",
+  kasikornBank: "Kasikorn Bank",
+  krungsriBank: "Bank of Ayudhya",
+  name: "Sumalee Chayakulkeeree",
+  name2: "Sumalee Chayakulkeeree (Pokakorn)",
+  orders1: "How do I place an order?",
+  orders1a:
+    "Currently, you can only order by directly contacting us by email or LINE. If you are using LINE, use the button below or search for @kloris (with @-sign) to begin chatting with us. If you are ordering via email, please send an email to",
+  orderEmail: "orders@kloriscosmetics.com",
+  orderIncludeHeader: "In your order, please include the following details:",
+  orderInclude1: "The names of items you wish to order",
+  orderInclude2: "Your name, address, and phone number.",
+  orderConfirm:
+    "After ordering, please wait for our confirmation, and pay for your order after receiving our order confirmation.",
+  shippingConfirm:
+    "We will begin shipping only once your payment has been confirmed.",
+  orderNotes:
+    "Note: If you message us outside of our office hours (8AM-6PM, Mon-Sun), you will be answered by an automated system. Do not worry! We have already received your message and our staff will get back to you during the office hours.",
+}
