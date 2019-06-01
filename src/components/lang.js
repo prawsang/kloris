14a4b6fded34dbd2c61ab29c92bf365aa6b01008
Prawsang <prawcha.p@gmail.com>
2019-06-02 00:44:33 +0700

    language switcher switches to current page

--- src/components/lang.js
+++ src/components/lang.js
@@ -5,9 +5,14 @@ import Link from "gatsby-link"
 
 const LanguageSelect = ({ langs, currentLangKey, defaultLangKey }) => {
   const altLangs = langs.filter(e => e !== defaultLangKey)
+  const location = window.location.pathname.split("/")
+  const url = location[location.length - 1]
+  console.log(url)
   return (
     <Link
-      to={currentLangKey !== defaultLangKey ? "/" : `/${altLangs[0]}`}
+      to={
+        currentLangKey !== defaultLangKey ? `/${url}` : `/${altLangs[0]}/${url}`
+      }
       className="lang"
     >
       <img src={currentLangKey !== defaultLangKey ? en : th} alt="" />
