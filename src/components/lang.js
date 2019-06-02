1145521e543adaf1cd77748b2c543ef5466004aa
Prawsang <prawcha.p@gmail.com>
2019-06-03 00:03:08 +0700

    fix build bugs

--- src/components/lang.js
+++ src/components/lang.js
@@ -4,24 +4,20 @@ import en from "../assets/web/en.png"
 import Link from "gatsby-link"
 
 class LanguageSelect extends React.Component {
-  state = {
-    url: "",
-  }
-  componentDidMount() {
-    const location = window.location.pathname.split("/")
-    this.setState({ url: location[location.length - 1] })
-  }
+  // state = {
+  //   url: "",
+  // }
+  // componentDidMount() {
+  //   const location = window.location.pathname.split("/")
+  //   this.setState({ url: location[location.length - 1] })
+  // }
   render() {
     const { defaultLangKey, currentLangKey, langs } = this.props
-    const { url } = this.state
+    // const { url } = this.state
     const altLangs = langs.filter(e => e !== defaultLangKey)
     return (
       <Link
-        to={
-          currentLangKey !== defaultLangKey
-            ? `/${url}`
-            : `/${altLangs[0]}/${url}`
-        }
+        to={currentLangKey !== defaultLangKey ? "/" : `/${altLangs[0]}`}
         className="lang"
       >
         <img src={currentLangKey !== defaultLangKey ? en : th} alt="" />
